7c6e2572a75a2a0d8ad63e87350eeead45cb35a9
Danzz Coding <muhammadramdani196453@gmail.com>
2022-09-02 22:27:46 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -9,8 +9,34 @@ var router  = express.Router();
 
 // Setting
 const creator = 'Danzz Coding'
+
 // Loghandler
-const loghandler = require('./handler');
+loghandler = {
+    error: {
+        status: false,
+        code: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        maintanied_by: `${creator}`
+    },
+    apikey: {
+    	status: false,
+    	code: 403,
+    	message: 'Forbiden, Invalid apikey, hubungi saya di whatsapp untuk mendapatkan apikey',
+    	maintanied_by: `${creator}`
+    },
+    noturl: {
+    	status: false,
+    	code: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	maintanied_by: `${creator}`,
+    },
+    nottext: {
+    	status: false,
+    	code: 403,
+    	message: 'Forbiden, Invlid text, masukkan parameter text',
+    	maintanied_by: `${creator}`,
+    }
+}
 
 // Feature
 
